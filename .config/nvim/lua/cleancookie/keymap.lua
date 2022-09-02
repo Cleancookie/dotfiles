86e981622d1c194c7c48e1f3c42a8bdc1ce96045
Cleancookie <pokemon999101@hotmail.co.uk>
2022-09-02 22:55:45 +0100

    🌟 Add PHP LSP support

--- .config/nvim/lua/cleancookie/keymap.lua
+++ .config/nvim/lua/cleancookie/keymap.lua
@@ -22,3 +22,9 @@ map('n', '<leader>p', ':Telescope find_files<CR>')
 map('n', '<leader>b', ':Telescope buffers<CR>')
 map('n', '<leader>e', ':Ex<CR>')
 
+local bufopts = { noremap=true, silent=true, buffer=bufnr }
+vim.keymap.set('n', '<leader>f', vim.diagnostic.open_float, bufopts)
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
--- .config/nvim/lua/cleancookie/packer.lua
+++ .config/nvim/lua/cleancookie/packer.lua
@@ -5,6 +5,7 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'morhetz/gruvbox'
+  use 'neovim/nvim-lspconfig'
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
 	  requires = { {'nvim-lua/plenary.nvim'} }
