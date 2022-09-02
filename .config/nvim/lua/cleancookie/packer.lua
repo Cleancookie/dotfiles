b51fc921151be802e734774038d6d2c128a3f030
Cleancookie <pokemon999101@hotmail.co.uk>
2022-09-02 21:03:34 +0100

    Add telescope to nvim

--- .config/nvim/lua/cleancookie/packer.lua
+++ .config/nvim/lua/cleancookie/packer.lua
@@ -4,5 +4,9 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
---   use 'morhetz/gruvbox'
+  use 'morhetz/gruvbox'
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
 end)
