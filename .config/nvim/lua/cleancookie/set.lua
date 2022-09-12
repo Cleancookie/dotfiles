e2796dd30c1c6a824d24c74aa52d9daafbe002b3
Alex Law <alex.law@global4.co.uk>
2022-09-12 12:58:14 +0100

    🐛 Remove undodir

--- .config/nvim/lua/cleancookie/set.lua
+++ .config/nvim/lua/cleancookie/set.lua
@@ -6,9 +6,11 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4 -- tab space for when shifting across
 vim.opt.smartindent = true -- try to auto indent when it can
 vim.opt.swapfile = false
-vim.opt.undodir = '~/.vim/undodir' -- Save the undos
+vim.opt.undodir = '~/.vim/undodir' -- Save the undos -- its not resolving ~ as home?
 vim.opt.undofile = false
 vim.opt.incsearch = true
 vim.opt.encoding = 'utf-8'
 vim.opt.number = true -- line numbers
 vim.opt.signcolumn = 'no' -- get rid of that grey bar on the left
+
+
