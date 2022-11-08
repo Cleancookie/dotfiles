d1c140712cd4e12962f00ecef49f18e8063aad45
Alex Law <alex.law@global4.co.uk>
2022-11-08 10:41:38 +0000

    🚀 AstroVim

--- .config/nvim/README.md
+++
@@ -1,5 +0,0 @@
-## Setup
-
-You will need https://github.com/wbthomason/packer.nvim
-
-You will also need https://www.npmjs.com/package/intelephense
--- .config/nvim/lua/cleancookie/init.lua
+++
@@ -1,7 +0,0 @@
-require('cleancookie/packer')
-require('cleancookie/lsp/php')
-require('cleancookie/lsp/volar')
-
-require('cleancookie/set')
-require('cleancookie/keymap')
-
