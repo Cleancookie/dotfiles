09c4b3e79e19bc40346cae7fe7d977f22dc6416b
Alex Law <alex.law@global4.co.uk>
2023-01-11 09:08:38 +0000

    ✨ neovim configs

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -42,7 +42,7 @@ local config = {
         options = {
                 opt = {
                         -- set to true or false etc.
-                        relativenumber = false, -- sets vim.opt.relativenumber
+                        relativenumber = true, -- sets vim.opt.relativenumber
                         number = true, -- sets vim.opt.number
                         spell = false, -- sets vim.opt.spell
                         signcolumn = "auto", -- sets vim.opt.signcolumn to auto
@@ -50,7 +50,7 @@ local config = {
                 },
                 g = {
                         mapleader = " ", -- sets vim.g.mapleader
-                        autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+                        autoformat_enabled = false, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
                         cmp_enabled = true, -- enable completion at start
                         autopairs_enabled = true, -- enable autopairs at start
                         diagnostics_enabled = true, -- enable diagnostics at start
